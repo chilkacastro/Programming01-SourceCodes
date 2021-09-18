9c685b645c786a651f75a2320ce50d8baabdee36
CHIL_CJ <70527669+chilkacastro@users.noreply.github.com>
2021-09-17 21:57:03 -0400

    HelloWorld

---
+++ HelloWorld/src/helloworld/HelloWorld.java
@@ -0,0 +1,50 @@
+/* one star means multi-line comments
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworld;         // two "/" is a single-line comment
+
+/** -> this is to document(documentation)
+ * two starts means documentation
+ * To print "HelloWorld" in the console
+ * @author Chilka Castro
+ */
+public class HelloWorld {   //OOP obligated: class object
+
+    /** method: a piece of code that can do a specific task
+     * in which, main() is a special method, that is always the starting 
+     * position of the project
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+//        System.out.println("Attribute       Value");   
+//        System.out.println();               //empty line
+//        System.out.println("Name:           Yi Wang");   
+//        System.out.println("Age:            33");   
+//        System.out.println("Nationality:    China");   
+//        System.out.println("Department:     Computer Science");   
+//        System.out.println("Position:       Lecturer");   
+        System.out.println("*********           ***           *           *");
+        System.out.println("*       *       *         *      ***        *   *");
+        System.out.println("*       *      *           *    *****      *     *");
+        System.out.println("*       *      *           *      *       *       *");
+        System.out.println("*       *      *           *      *      *         *");
+        System.out.println("*       *      *           *      *       *       *");
+        System.out.println("*       *      *           *      *        *     *");
+        System.out.println("*       *       *         *       *         *   *");
+        System.out.println("*********           ***           *           *");
+   
+    }
+    public static void sum() {
+        
+    }
+  
+    public static void avg() {
+        int assignmentScore;             // score1 is bad
+        int finalExamScore;              // score2 is bad; also have a pattern
+        
+    }
+    
+      
+}
